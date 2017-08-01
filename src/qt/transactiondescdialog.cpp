9241c85d32ae18769d63045ee0a32fd8686529b7
wangliren <wangliren01@gmail.com>
2017-08-01 22:55:05 +0800

    Royal Bitcoin is truly decentralized bitcoin.

---
+++ src/qt/transactiondescdialog.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2011-2013 The Bitcoin Core developers
+// Copyright (c) 2017 earthledger
+// Copyright (c) 2017 renminhuijin
+// Copyright (c) 2017 genyuanlian
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "transactiondescdialog.h"
+#include "ui_transactiondescdialog.h"
+
+#include "transactiontablemodel.h"
+
+#include <QModelIndex>
+
+TransactionDescDialog::TransactionDescDialog(const QModelIndex &idx, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TransactionDescDialog)
+{
+    ui->setupUi(this);
+    QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
+    ui->detailText->setHtml(desc);
+}
+
+TransactionDescDialog::~TransactionDescDialog()
+{
+    delete ui;
+}
---
+++ src/support/cleanse.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2017 earthledger
+// Copyright (c) 2017 renminhuijin
+// Copyright (c) 2017 genyuanlian
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "cleanse.h"
+
+#include <openssl/crypto.h>
+
+void memory_cleanse(void *ptr, size_t len)
+{
+    OPENSSL_cleanse(ptr, len);
+}
